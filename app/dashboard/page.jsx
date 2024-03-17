b16a1e93932412f3acbd6863ee8e9cbbf47b4e27
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-03-17 17:14:08 +0900

    ⚡️ card, chart, transactions, rightbar, footer 추가

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import Card from "../ui/dashboard/card/Card";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Rightbar from "../ui/dashboard/rightbar/Rightbar";
+import Transactions from "../ui/dashboard/transactions/Transactions";
+import Chart from "../ui/dashboard/chart/Chart";
 
 const Dashboard = () => {
-  return <div>Dashboard</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transactions />
+        <Chart />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
