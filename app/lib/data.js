c512c52aa41d41cf30f3c624555c74d744727756
YeoDaSeul4355 <125419623+YeoDaSeul4355@users.noreply.github.com>
2024-03-31 21:27:34 +0900

    💡 설명 주석 추가

--- app/lib/data.js
+++ app/lib/data.js
@@ -1,18 +1,30 @@
 import { Product, User } from "./models";
 import { connectToDB } from "./utils";
 
+// user데이터 가져오는 함수. q는 검색어, page는 페이지네이션. (props)
 export const fetchUsers = async (q, page) => {
+  // 검색어를 대소문자 구분하지 않는 정규식 반환
   const regex = new RegExp(q, "i");
 
+  // 페이지 당 노출 시킬 user 개수 (일단 2로 설정)
   const ITEM_PER_PAGE = 2;
 
   try {
+    // DB와 연결
     connectToDB();
+
+    // 검색어를 포함하는 사용자의 총 수를 계산.(페이지네이션)
     const count = await User.find({ username: { $regex: regex } }).count();
+
+    // 검색어 포함하는 사용자 찾기. limit로 결과를 페이지 당 아이템 수로 제한. skip으로 요청된 페이지에 해당하는 아이템을 스킵
     const users = await User.find({ username: { $regex: regex } })
       .limit(ITEM_PER_PAGE)
       .skip(ITEM_PER_PAGE * (page - 1));
+
+    // 검색된 사용자의 수와 사용자 목록 반환
     return { count, users };
+
+    // catch문으로 에러처리
   } catch (err) {
     console.log(err);
     throw new Error("Failed to fetch user!");
--- app/ui/dashboard/search/Search.jsx
+++ app/ui/dashboard/search/Search.jsx
@@ -3,25 +3,33 @@
 import { MdSearch } from "react-icons/md";
 import styles from "./search.module.css";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
-import {} from "next/router";
 import { useDebouncedCallback } from "use-debounce";
 
 const Search = ({ placeholder }) => {
+  // 현재 URL의 쿼리 문자열을 가져옴
   const searchParams = useSearchParams();
+  // 현재 경로를 가져옴
   const pathname = usePathname();
+  // 라우터 훅에서 replace 함수를 가져옴. replace함수는 브라우저의 url을 변경하는 함수임.
   const { replace } = useRouter();
 
-  // 서치 핸들링
+  // 서치 핸들링 함수. useDebouncedCallback 훅을 사용하여 입력을 디바운싱함. 검색어 입력을 300ms마다 호출
   const handleSearch = useDebouncedCallback((e) => {
+    // 현재 url쿼리 문자열을 가져와 URLSearchParams 객체를 생성함. 이 객체를 사용하여 URL의 쿼리 문자열을 파싱하고 조작할 수 있음.
     const params = new URLSearchParams(searchParams);
 
+    // page 쿼리 파라미터를 1로 설정함. 검색어가 변경될 때마다 page를 1로 설정
     params.set("page", 1);
 
+    // 입력값의 길이가 2이상임을 확인하고 값이 있으면 q를 해당 검색어로 설정.
     if (e.target.value) {
       e.target.value.length > 2 && params.set("q", e.target.value);
     } else {
+      // 입력값 없으면 q제거
       params.delete("q");
     }
+
+    // url를 replace함수를 통해 업데이트 함. pathname은 현재 경로, params에는 새로운 쿼리 문자열이 포함되어있음.
     replace(`${pathname}?${params}`);
   }, 300);
 
